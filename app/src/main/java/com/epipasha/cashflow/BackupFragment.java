65fddf499751db2e44db2061fd8fa7eb32f26295
Pavel <epipasha@gmail.com>
2017-09-28 13:01:38 +0300

    Change lists design

--- app/src/main/java/com/epipasha/cashflow/BackupFragment.java
+++ app/src/main/java/com/epipasha/cashflow/BackupFragment.java
@@ -1,6 +1,5 @@
 package com.epipasha.cashflow;
 
-
 import android.app.Fragment;
 import android.content.ContentValues;
 import android.database.Cursor;
@@ -19,44 +18,39 @@ import com.epipasha.cashflow.db.CashFlowContract.CategoryEntry;
 import com.epipasha.cashflow.db.CashFlowContract.OperationEntry;
 import com.epipasha.cashflow.db.CashFlowDbHelper;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.Iterator;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class BackupFragment extends Fragment {
-
     private static final String[] TABLES = new String[]{
             AccountEntry.TABLE_NAME,
             CategoryEntry.TABLE_NAME,
             OperationEntry.TABLE_NAME,
             AccountBalanceEntry.TABLE_NAME,
-            CategoryCostEntry.TABLE_NAME};
+            CategoryCostEntry.TABLE_NAME
+    };
 
     public BackupFragment() {
-        // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+            Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_backup, container, false);
 
-
-
-        Button share = (Button)v.findViewById(R.id.share);
+        Button share = (Button) v.findViewById(R.id.share);
         share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
                     JSONObject db = new JSONObject();
-                    for (String table: TABLES) {
+                    for (String table : TABLES) {
                         try {
                             db.put(table, exportDb(table));
                         } catch (JSONException e) {
@@ -75,7 +69,7 @@ public class BackupFragment extends Fragment {
             }
         });
 
-        Button restore = (Button)v.findViewById(R.id.restore);
+        Button restore = (Button) v.findViewById(R.id.restore);
         restore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -100,15 +94,15 @@ public class BackupFragment extends Fragment {
         return v;
     }
 
-    private JSONArray exportDb(String myTable){
+    private JSONArray exportDb(String myTable) {
 
         CashFlowDbHelper helper = new CashFlowDbHelper(getActivity());
         SQLiteDatabase db = helper.getReadableDatabase();
 
         String searchQuery = "SELECT  * FROM " + myTable;
-        Cursor cursor = db.rawQuery(searchQuery, null );
+        Cursor cursor = db.rawQuery(searchQuery, null);
 
-        JSONArray resultSet     = new JSONArray();
+        JSONArray resultSet = new JSONArray();
 
         cursor.moveToFirst();
         while (!cursor.isAfterLast()) {
@@ -116,18 +110,17 @@ public class BackupFragment extends Fragment {
             int totalColumn = cursor.getColumnCount();
             JSONObject rowObject = new JSONObject();
 
-            for( int i=0 ;  i< totalColumn ; i++ ){
-                if( cursor.getColumnName(i) != null ){
-                    try{
-                        if( cursor.getString(i) != null ){
-                            Log.d("TAG_NAME", cursor.getString(i) );
-                            rowObject.put(cursor.getColumnName(i) ,  cursor.getString(i) );
+            for (int i = 0; i < totalColumn; i++) {
+                if (cursor.getColumnName(i) != null) {
+                    try {
+                        if (cursor.getString(i) != null) {
+                            Log.d("TAG_NAME", cursor.getString(i));
+                            rowObject.put(cursor.getColumnName(i), cursor.getString(i));
+                        } else {
+                            rowObject.put(cursor.getColumnName(i), "");
                         }
-                        else{
-                            rowObject.put( cursor.getColumnName(i) ,  "" );
-                        }
-                    }catch( Exception e ){
-                        Log.d("TAG_NAME", e.getMessage()  );
+                    } catch (Exception e) {
+                        Log.d("TAG_NAME", e.getMessage());
                     }
                 }
             }
@@ -135,30 +128,30 @@ public class BackupFragment extends Fragment {
             cursor.moveToNext();
         }
         cursor.close();
-        Log.d("TAG_NAME", resultSet.toString() );
+        Log.d("TAG_NAME", resultSet.toString());
         return resultSet;
 
     }
 
-    private void importDb(JSONObject obj){
+    private void importDb(JSONObject obj) {
 
         CashFlowDbHelper helper = new CashFlowDbHelper(getActivity());
         SQLiteDatabase db = helper.getWritableDatabase();
 
-        for (String table: TABLES) {
+        for (String table : TABLES) {
             try {
                 JSONArray rows = obj.getJSONArray(table);
-                for (int i=0; i<rows.length(); i++) {
+                for (int i = 0; i < rows.length(); i++) {
                     JSONObject row = rows.getJSONObject(i);
 
                     ContentValues values = new ContentValues();
 
                     Iterator<String> iterator = row.keys();
-                    while(iterator.hasNext()){
+                    while (iterator.hasNext()) {
                         String key = iterator.next();
                         try {
                             values.put(key, Integer.parseInt((String) row.get(key)));
-                        }catch (Exception e){
+                        } catch (Exception e) {
                             values.put(key, (String) row.get(key));
                         }
                     }
