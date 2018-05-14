76b8286435ec5f8c521c4df3857d1a30d1e89df4
Pavel <epipasha@gmail.com>
2018-05-14 21:06:27 +0300

    Add popup menu to operation list

--- app/src/main/java/com/epipasha/cashflow/adapters/OperationAdapter.java
+++ app/src/main/java/com/epipasha/cashflow/adapters/OperationAdapter.java
@@ -1,21 +1,27 @@
 package com.epipasha.cashflow.adapters;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
+import com.epipasha.cashflow.MainActivity;
 import com.epipasha.cashflow.R;
 import com.epipasha.cashflow.data.CashFlowContract;
 import com.epipasha.cashflow.activities.DetailOperationActivity;
+import com.epipasha.cashflow.data.CashFlowContract.OperationEntry;
 import com.epipasha.cashflow.objects.OperationType;
 
 import java.text.SimpleDateFormat;
@@ -46,13 +52,13 @@ public class OperationAdapter extends Adapter<OperationAdapter.OperationHolder> 
     @Override
     public Cursor swapCursor(Cursor c) {
         if(c != null){
-            idIndex = c.getColumnIndex(CashFlowContract.OperationEntry._ID);
-            dateIndex = c.getColumnIndex(CashFlowContract.OperationEntry.COLUMN_DATE);
-            accountIndex = c.getColumnIndex(CashFlowContract.OperationEntry.SERVICE_COLUMN_ACCOUNT_TITLE);
-            categoryIndex = c.getColumnIndex(CashFlowContract.OperationEntry.SERVICE_COLUMN_CATEGORY_TITLE);
-            repAccountIndex = c.getColumnIndex(CashFlowContract.OperationEntry.SERVICE_COLUMN_RECIPIENT_ACCOUNT_TITLE);
-            typeIndex = c.getColumnIndex(CashFlowContract.OperationEntry.COLUMN_TYPE);
-            sumIndex = c.getColumnIndex(CashFlowContract.OperationEntry.COLUMN_SUM);
+            idIndex = c.getColumnIndex(OperationEntry._ID);
+            dateIndex = c.getColumnIndex(OperationEntry.COLUMN_DATE);
+            accountIndex = c.getColumnIndex(OperationEntry.SERVICE_COLUMN_ACCOUNT_TITLE);
+            categoryIndex = c.getColumnIndex(OperationEntry.SERVICE_COLUMN_CATEGORY_TITLE);
+            repAccountIndex = c.getColumnIndex(OperationEntry.SERVICE_COLUMN_RECIPIENT_ACCOUNT_TITLE);
+            typeIndex = c.getColumnIndex(OperationEntry.COLUMN_TYPE);
+            sumIndex = c.getColumnIndex(OperationEntry.COLUMN_SUM);
         }
 
         return super.swapCursor(c);
@@ -100,7 +106,7 @@ public class OperationAdapter extends Adapter<OperationAdapter.OperationHolder> 
         }
     }
 
-    class OperationHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    class OperationHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener, PopupMenu.OnMenuItemClickListener {
         TextView operationDateView;
         TextView operationAccountView;
         TextView operationCategoryView;
@@ -116,6 +122,7 @@ public class OperationAdapter extends Adapter<OperationAdapter.OperationHolder> 
             operationTypeImageView = (ImageView) itemView.findViewById(R.id.operation_list_item_type);
 
             itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(this);
         }
 
         @Override
@@ -123,14 +130,68 @@ public class OperationAdapter extends Adapter<OperationAdapter.OperationHolder> 
             int adapterPosition = getAdapterPosition();
             mCursor.moveToPosition(adapterPosition);
 
-            int idIndex = mCursor.getColumnIndex(CashFlowContract.OperationEntry._ID);
+            int idIndex = mCursor.getColumnIndex(OperationEntry._ID);
             int id = mCursor.getInt(idIndex);
 
             Intent i = new Intent(mContext, DetailOperationActivity.class);
 
-            Uri uri = CashFlowContract.OperationEntry.buildOperationUriWithId(id);
+            Uri uri = OperationEntry.buildOperationUriWithId(id);
             i.setData(uri);
             mContext.startActivity(i);
+        }
+
+        @Override
+        public boolean onLongClick(View view) {
+
+            PopupMenu popupMenu = new PopupMenu(mContext, view);
+            popupMenu.inflate(R.menu.popup_list_item_operation);
+            popupMenu.setOnMenuItemClickListener(this);
+            popupMenu.show();
+
+            return true;
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+
+            if(menuItem.getItemId() == R.id.action_delete_operation){
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                builder.setMessage(R.string.dialog_delete_operation)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                                // Retrieve the id of the task to delete
+                                int adapterPosition = getAdapterPosition();
+                                mCursor.moveToPosition(adapterPosition);
+
+                                int idIndex = mCursor.getColumnIndex(OperationEntry._ID);
+                                int operationId = mCursor.getInt(idIndex);
+
+                                // Build appropriate uri with String row id appended
+                                String stringId = Integer.toString(operationId);
+                                Uri uri = OperationEntry.CONTENT_URI;
+                                uri = uri.buildUpon().appendPath(stringId).build();
+
+                                // COMPLETED (2) Delete a single row of data using a ContentResolver
+                                mContext.getContentResolver().delete(uri, null, null);
+
+                                // COMPLETED (3) Restart the loader to re-query for all tasks after a deletion
+                                notifyDataSetChanged();
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                notifyDataSetChanged();
+                            }
+                        });
+                // Create the AlertDialog object and return it
+                builder.create().show();
+
+                return true;
+            }
+
+            return false;
         }
     }
 }
